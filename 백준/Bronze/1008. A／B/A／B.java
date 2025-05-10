3068cd03643c1347f15c44f0441c9185bea0d2fb
Sykim <iisdf@naver.com>
2025-05-11 05:31:29 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17996 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+	public static void main(String args[]) {
+		Scanner scr = new Scanner(System.in);
+		double A = scr.nextDouble();
+		double B = scr.nextDouble();
+		System.out.println(A/B);
+	}
+}
