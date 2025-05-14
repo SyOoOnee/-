af0dabbad48a554e61c64b2059ef1c896d077e7e
Sykim <iisdf@naver.com>
2025-05-14 16:13:36 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,24 +1,25 @@
 import java.util.*;
+import java.io.*;
 
 public class Main{
-	public static void main(String args[]) {
-		Scanner sr = new Scanner(System.in);
-		int A = sr.nextInt();
-		int B = sr.nextInt();
-		int C = sr.nextInt();
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt (br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
 		
 		int result = A*B*C;
+				
+		String rt = Integer.toString(result);
 		
-		String str = Integer.toString(result);
+		int[] count= new int[10];
 		
-		int [] count = new int[10];
-		
-		for(int i=0; i<str.length(); i++) {
-			int digit = str.charAt(i) - '0';
+		for(int i=0; i<rt.length(); i++) {
+			int digit = rt.charAt(i) - '0';
 			count[digit]++;
 		}
-		
-		for(int i=0; i<count.length; i++)
+		for(int i=0; i<count.length; i++) {
 			System.out.println(count[i]);
+		}
 	}
 }
